2c3220ded0eef8aebfc661be61f21c9295b1892d
Eduardo Torres <fucckthizz@gmail.com>
2026-02-12 12:02:25 -0600

    Fix: Correccion IDOR en views.py
    Validacion en views.py condicional de public_id

--- orders/views.py
+++ orders/views.py
@@ -4,8 +4,13 @@ from .models import Order
 from django.http import JsonResponse
 
 
-def order_success(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
+def order_success(request, public_id):
+    order = get_object_or_404(Order, public_id=public_id)
+
+    session_public_id = request.session.get("last_order_public_id")
+
+    if not session_public_id or str(order.public_id) != session_public_id:
+        return JsonResponse({"error": "Unauthorized"}, status=403)
 
     # 🔥 Vaciar carrito cuando el usuario regresa de Stripe
     _save_cart(request, {})
@@ -20,6 +25,12 @@ def order_success(request, order_id):
     )
 
 
-def order_status(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
+def order_status(request, public_id):
+    order = get_object_or_404(Order, public_id=public_id)
+
+    session_public_id = request.session.get("last_order_public_id")
+
+    if session_public_id != str(order.public_id):
+        return JsonResponse({"error": "Unauthorized"}, status=403)
+
     return JsonResponse({"status": order.status})
