712fb89054b1f70d36835978d21c4f0160c7e314
Eduardo Torres <fucckthizz@gmail.com>
2026-02-19 11:35:00 -0600

    Feat: New model
    Payments model create idempotence

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class StripeEvent(models.Model):
+    stripe_event_id = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.stripe_event_id
